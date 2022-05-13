7675e4007d91f6ff2cfe9b88626b388c1692fc33
TahirHNihal <tahirhnihal29@gmail.com>
2022-05-13 16:33:24 +0600

    upload

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-let text = 'Nihal';
-Pattern = /Haq47/i;
-console.log(patter);
+// # Create an email pattern
+
+let email = "freelancermamun103@gmail.com";
+let emailPattern = /^[a-z0-9._-]{1,}@(gmail|yahoo|icloud|outlook)\.com$/;
+console.log(emailPattern.test(email));
+
+//# Create a  username pattern
+
+let userName = "tahir_h_nihal_29";
+let uNamePatter = /^[a-z0-9_!@#$%^&*]{6,15}/;
+console.log(uNamePatter.test(userName));
+
+// # Create a Bangladeshi phone number pattern
+
+let bdNumber = "01911497396";
+let bdNumPattern = /^(\+8801|01)[0-9]{9}$/;
+console.log(bdNumPattern.test(bdNumber));
+
+// # Create a password pattern
+
+let pass = "tahirpass250";
+let passPattern = /^[a-z0-9!@#$%^&*()_+|":;`~]{8,}$/;
+console.log(passPattern.test(pass));
+
+// # Create a zipcode pattern
+
+let zipCode = "1230";
+let zipCodePattern = /^[0-9]{3,10}$/;
+console.log(zipCodePattern.test(zipCode));
